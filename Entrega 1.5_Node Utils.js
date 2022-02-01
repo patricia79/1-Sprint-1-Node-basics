9c449817144aa1a2d56244dec8070a4fe1d03bd9
Patricia <waltraute@hotmail.com>
2022-02-01 10:17:34 +0100

    fet

--- Entrega 1.5_Node Utils.js
+++ Entrega 1.5_Node Utils.js
@@ -4,18 +4,37 @@ Crea una funció que imprimeixi recursivament un missatge per la consola amb dem
 - T'has fet una mica de lio amb l'execucio del N1 E1, no sé per què l'executes amb un setTimeout(). 
 A més tot es diu missatge: la funcio, els paràmetres, un callback que no funciona..*/
 
-function missatge(missatge) {
+let missatge = () => {
+  setInterval(() => {
+    console.log("Hola");
+  }, 1000);
+};
+missatge();
 
-    console.log(`Aquest és el ${missatge}`);
-    setTimeout(missatge, 1000, missatge);
-}
-setTimeout(missatge, 1000, "missatge de Nadal");
 
 
 
-/* N1 Ex2 ok
-Crea una funció que, en executar-la, escrigui una frase en un fitxer.*/
 
+
+
+
+
+/* N1 Ex2 
+Crea una funció que, en executar-la, escrigui una frase en un fitxer.
+
+//Ejercicio 2
+const message = "Hello, I come from another World!";
+
+const writeMessageInAnotherFile = async () => {
+  try {
+    await fsp.writeFile("someText.txt", message);
+    console.log("Nivel 1, ejercicio 2:");
+    console.log("File created");
+  } catch (err) {
+    console.error(err);
+  }
+};
+writeMessageInAnotherFile();
 var fs = require('fs');
 
 function fraseFitxer(missatge) {
@@ -27,7 +46,7 @@ fraseFitxer("missatge de Nadal");
 
 
 /* N1 Ex3 ok
-Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.*/
+Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
 
 function contFitxer(fitxer) {
 
@@ -37,11 +56,46 @@ function contFitxer(fitxer) {
 }
 
 contFitxer('fitxerN1Ex2.txt');
+//Ejercicio 3
+const showMessageFromAnotherFile = async () => {
+    try {
+      const data = await fsp.readFile("./someText.txt", "utf-8");
+      console.log("Nivel 1, ejercicio 3:");
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  showMessageFromAnotherFile();
 
-
-/* N2 Ex1 no
+ N2 Ex1 no
 Crea una funció que comprimeixi el fitxer del nivell 1.
-*/
+
+//Nivel 2
+console.log("Nivel 2:");
+
+const compFile = () => {
+  fs.createReadStream("someText.txt")
+    .pipe(zlib.createGzip())
+    .pipe(fs.createWriteStream("someText.txt.gz"));
+  console.log("File Compressed.");
+};
+
+compFile();
+
+const showFilesFromDir = () => {
+  fs.readdir("/users", (err, files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
+};
+
+showFilesFromDir();
+
+
+
 
 var zlib = require('zlib');
 var fs = require('fs');
@@ -54,7 +108,7 @@ function compFitxer(fitxer, fitxerComp) {
     llegir.pipe(gzip).pipe(escriure);
 }
 
-compFitxer('fitxerN1Ex2.txt', 'fitxerN1Ex2.txt.zip');
+compFitxer('fitxerN1Ex2.txt', 'fitxerN1Ex2.txt.zip');*/
 
 /* N2 Ex2 no
 
@@ -62,30 +116,3 @@ Crea una funció que llisti per la consola el contingut del directori d'usuari d
 utilizant Node Child Processes.
  - La segona part del N2 ha de mostrar la carpeta de l'usuari (de windows o el que sigui), 
  no la carpeta on esta l'script!*/
-
-function contDirect(comando) {
-
-    const { exec } = require('child_process');
-
-    exec(comando, (err, stdout, stderr) => {
-
-        if (err) {
-
-            console.error(err);
-
-            return;
-        }
-        console.log(stdout);
-    });
-}
-
-contDirect('dir');
-
-/* N3 Ex1
-Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 respectivament, a partir del
-fitxer del nivell 1
-Crea una funció que guardi els fitxers del punt anterior, ara encriptats amb l'algorisme aes-192-cbc,
-i esborri els fitxers inicials
-Crea una altra funció que desencripti i descodifiqui els fitxers de l'apartat anterior tornant a
-generar una còpia de l'inicial
-Inclou un README amb instruccions per a l'execució de cada part*/
