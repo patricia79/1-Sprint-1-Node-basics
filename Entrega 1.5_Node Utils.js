e0a97c62981cda854aeaccdc506b6b1c363a7796
Patricia <waltraute@hotmail.com>
2022-02-01 10:25:02 +0100

    fet

--- Entrega 1.5_Node Utils.js
+++ Entrega 1.5_Node Utils.js
@@ -1,8 +1,6 @@
 
-/* N1 Ex1 no
-Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.
-- T'has fet una mica de lio amb l'execucio del N1 E1, no sé per què l'executes amb un setTimeout(). 
-A més tot es diu missatge: la funcio, els paràmetres, un callback que no funciona..*/
+/* N1 Ex1 
+Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.*/
 
 let missatge = () => {
   setInterval(() => {
@@ -12,38 +10,21 @@ let missatge = () => {
 missatge();
 
 
-
-
-
-
-
-
-
 /* N1 Ex2 
-Crea una funció que, en executar-la, escrigui una frase en un fitxer.
+Crea una funció que, en executar-la, escrigui una frase en un fitxer.*/
 
-//Ejercicio 2
-const message = "Hello, I come from another World!";
+let missatgeN1E2 = "Aquest és el missatge de Nadal";
 
-const writeMessageInAnotherFile = async () => {
+let missatgeN1E2 = async () => {
   try {
-    await fsp.writeFile("someText.txt", message);
-    console.log("Nivel 1, ejercicio 2:");
-    console.log("File created");
+    await fsp.writeFile("someText.txt", missatgeN1E2);
+    console.log("N1E2:");
+    console.log("Arxiu creat");
   } catch (err) {
     console.error(err);
   }
 };
-writeMessageInAnotherFile();
-var fs = require('fs');
-
-function fraseFitxer(missatge) {
-
-    fs.writeFile('fitxerN1Ex2.txt', `Aquest es el ${missatge}`, function (err) { });
-}
-
-fraseFitxer("missatge de Nadal");
-
+missatgeN1E2();
 
 /* N1 Ex3 ok
 Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
@@ -56,23 +37,12 @@ function contFitxer(fitxer) {
 }
 
 contFitxer('fitxerN1Ex2.txt');
-//Ejercicio 3
-const showMessageFromAnotherFile = async () => {
-    try {
-      const data = await fsp.readFile("./someText.txt", "utf-8");
-      console.log("Nivel 1, ejercicio 3:");
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  showMessageFromAnotherFile();
+
+
 
  N2 Ex1 no
 Crea una funció que comprimeixi el fitxer del nivell 1.
 
-//Nivel 2
 console.log("Nivel 2:");
 
 const compFile = () => {
@@ -93,22 +63,6 @@ const showFilesFromDir = () => {
 };
 
 showFilesFromDir();
-
-
-
-
-var zlib = require('zlib');
-var fs = require('fs');
-
-function compFitxer(fitxer, fitxerComp) {
-
-    const llegir = fs.createReadStream(fitxer);
-    const escriure = fs.createWriteStream(fitxerComp);
-    const gzip = zlib.createGzip();
-    llegir.pipe(gzip).pipe(escriure);
-}
-
-compFitxer('fitxerN1Ex2.txt', 'fitxerN1Ex2.txt.zip');*/
 
 /* N2 Ex2 no
 
