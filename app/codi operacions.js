d826ee07b677643aef06b7bbe5ac43575162edb0
Patricia <waltraute@hotmail.com>
2022-02-01 10:50:14 +0100

    fet

--- app/codi operacions.js
+++ app/codi operacions.js
@@ -1,20 +1,29 @@
 class Calculadora {
-    calcularSuma(a, b) {
-        
+    calcularSuma = (a, b) => {
+
+        if (a !== "nombre" || b !== "nombre")
+            return "no es poden fer les operacions sense nombres";
+
         return a + b;
-    }
+    };
 
-    calcularResta(a, b) {
+    calcularResta = (a, b) => {
+        if (!a) return b;
+        if (!b) return a;
+
         return a - b;
-    }
+    };
+    calcularMultiplicacio = (a, b) => {
 
-    calcularMultiplicacio(a, b) {
         return a * b;
-    }
+    };
 
-    calcularDivisio(a, b) {
+    calcularDivisio = (a, b) => {
+        if (b === 0) return "No es pot fer l'operació entre 0";
         return a / b;
-    }
+    };
 }
 
 module.exports = Calculadora;
+
+
