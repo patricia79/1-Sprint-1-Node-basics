19ad3c5ffcb3143c922da619fa51b6f11a539d44
Patricia <waltraute@hotmail.com>
2022-02-02 11:04:40 +0100

    fet 02022022

--- Entrega 1.2_ Classes _ Arrow Functions.js
+++ Entrega 1.2_ Classes _ Arrow Functions.js
@@ -1,14 +1,11 @@
 
 /* Nivell 1 Exercici 1 
-Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.*/
-/* Nivell 1 Exercici 1   no estàs mostrant el resultat d'una funció, sino executant una funció que mostra coses*/
+Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.
+ No estàs mostrant el resultat d'una funció, sino executant una funció que mostra coses*/
 
-((a, b) =>
-    console.log(a + b)
-)(1, 2);
+console.log(((a, b) => a + b)(1, 2));
 
-
-/* Nivell 2 Exercici 1  OK
+/* Nivell 2 Exercici 1 
 Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a
  valor el paràmetre rebut.*/
 
