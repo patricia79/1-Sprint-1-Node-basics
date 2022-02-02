6b1d0ea84710a35482d2d075b5914b060e761e21
Patricia <waltraute@hotmail.com>
2022-02-02 09:42:03 +0100

    fet

--- Entrega 1.2_ Classes _ Arrow Functions.js
+++ Entrega 1.2_ Classes _ Arrow Functions.js
@@ -1,14 +1,14 @@
 
 /* Nivell 1 Exercici 1 
-
 Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.*/
+/* Nivell 1 Exercici 1   no estàs mostrant el resultat d'una funció, sino executant una funció que mostra coses*/
 
 ((a, b) =>
     console.log(a + b)
 )(1, 2);
 
 
-/* Nivell 2 Exercici 1 
+/* Nivell 2 Exercici 1  OK
 Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a
  valor el paràmetre rebut.*/
 
@@ -24,6 +24,7 @@ func2("Hola");
 Crea una classe Persona que rebi un paràmetre 'nom' al ser instanciada. 
 La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. 
 Invoca el mètode dirNom des de fora de la classe.*/
+// Nivell 2 Exercici 2 El mètode dirNom() ha de dir el nom del paràmetre que té l'objecte, no un paràmetre extra!   
 
 class Persona {
 
