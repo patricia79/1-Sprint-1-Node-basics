637023d029d7e807987fb29f8f4aa90b00ecb4db
Patricia <waltraute@hotmail.com>
2022-03-04 03:58:40 +0100

    class abstract 04-03

--- Entrega 1.2_ Classes _ Arrow Functions.js
+++ Entrega 1.2_ Classes _ Arrow Functions.js
@@ -1,4 +1,3 @@
-
 /* Nivell 1 Exercici 1 
 Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.*/
 
@@ -8,13 +7,12 @@ console.log(((a, b) => a + b)(1, 2));
 Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a
  valor el paràmetre rebut.*/
 
-var func2 = y => {
-    var myObj = { atrib1: y };
-    return myObj;
-}
+var func2 = (y) => {
+  var myObj = { atrib1: y };
+  return myObj;
+};
 
 func2("Hola");
-
 
 /* Nivell 2 Exercici 2 
 Crea una classe Persona que rebi un paràmetre 'nom' al ser instanciada. 
@@ -22,40 +20,41 @@ La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre '
 Invoca el mètode dirNom des de fora de la classe.*/
 
 class Persona {
+  constructor(nom) {
+    this.nom = nom;
+  }
 
-    constructor(nom) {
-        this.nom = nom;
-    }
-
-    dirNom() {
-        console.log(this.nom);
-    }
+  dirNom() {
+    console.log(this.nom);
+  }
 }
 var patricia = new Persona("Pol");
 patricia.dirNom();
 
-
-
-
 /*  Nivell 3 Exercici 1 
 Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta.
-Invoca-la amb diferents definicions.*/
+Invoca-la amb diferents definicions.
+Las clases abstractas se pueden definir como clases que no se pueden instanciar, es decir, cuya referencia 
+de objeto no se puede crear y contiene dentro de ella, uno o más métodos abstractos.
+Un método abstracto es un método que solo puede declararse pero no tiene implementación. Las clases abstractas
+ deben heredarse y requieren subclases para proporcionar implementaciones para el método declarado en la clase
+ abstracta.
+  tanto el prototipo de la instancia de un objeto (al cual se accede mediante Object.getPrototypeOf(obj), o
+  a través de la propiedad __proto__) como el prototipo que contiene el constructor (que se encuentra en la 
+    propiedad prototype del constructor) hacen referencia al mismo objeto.*/
 
-let perro = {
-nombre:"Toby",
-pelo:"duro",
-edad:6,
-ficha_completa:function () {
-    return console.log(this.nombre), console.log(this.pelo), console.log(this.edad);
+class Gat {
+  // funció abstracta i ojo q no es pot instanciar, surt error
+  constructor() {
+    if (this.constructor === Gat) {
+      throw new Error("La classe abstracta no pot ser instanciada.");
+    }
+  }
 }
-}
-console.log(perro.ficha_completa());
-
-// Escriu una function creadora d'objectes
-
-function creacio_objecte()
-{
-    let gat = new Object();
-    return console.log(gat.nom), console.log(gat.pel);
-}
-creacio_objecte ("Garfield", "curt");
+const gatPrototype = () => {
+  gatet = Object.create(Gat.prototype, { constructor: { value: Gat } });
+  return gatet;
+};
+const garfield = gatPrototype();
+console.log(garfield);
+console.log(gatet);
