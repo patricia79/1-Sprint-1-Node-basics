70bcf7ef2d3dd7cea01c3f237bf33302a827171d
Patricia <waltraute@hotmail.com>
2022-03-04 03:57:39 +0100

    class abstracta 04-03

--- Entrega 1.2_ Classes _ Arrow Functions.js
+++ Entrega 1.2_ Classes _ Arrow Functions.js
@@ -1,7 +1,6 @@
 
 /* Nivell 1 Exercici 1 
-Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.
- No estàs mostrant el resultat d'una funció, sino executant una funció que mostra coses*/
+Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres.*/
 
 console.log(((a, b) => a + b)(1, 2));
 
@@ -21,7 +20,7 @@ func2("Hola");
 Crea una classe Persona que rebi un paràmetre 'nom' al ser instanciada. 
 La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. 
 Invoca el mètode dirNom des de fora de la classe.*/
-// Nivell 2 Exercici 2 El mètode dirNom() ha de dir el nom del paràmetre que té l'objecte, no un paràmetre extra!   
+
 class Persona {
 
     constructor(nom) {
@@ -42,6 +41,21 @@ patricia.dirNom();
 Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta.
 Invoca-la amb diferents definicions.*/
 
+let perro = {
+nombre:"Toby",
+pelo:"duro",
+edad:6,
+ficha_completa:function () {
+    return console.log(this.nombre), console.log(this.pelo), console.log(this.edad);
+}
+}
+console.log(perro.ficha_completa());
 
+// Escriu una function creadora d'objectes
 
-
+function creacio_objecte()
+{
+    let gat = new Object();
+    return console.log(gat.nom), console.log(gat.pel);
+}
+creacio_objecte ("Garfield", "curt");
