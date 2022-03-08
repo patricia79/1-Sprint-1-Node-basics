c546db389dde0830cbe8da3bc1c61c2ddc8395c9
Patricia <waltraute@hotmail.com>
2022-03-09 00:09:57 +0100

    fet

--- app/codi N1Ex1.js
+++ app/codi N1Ex1.js
@@ -1,20 +1,109 @@
+/*Crea un arxiu amb les funcions sumar, restar, multiplicar i dividir dos o més operands. 
+Testeja la correcta execució d'aquestes funcions.*/
 
-let getEmployee = id => {
+class Calculadora {
+    calcularSuma = (a, b) => {
+
+        if (a !== "nombre" || b !== "nombre")
+            return "no es poden fer les operacions sense nombres";
+
+        return a + b;
+    };
+
+    calcularResta = (a, b) => {
+        if (!a) return b;
+        if (!b) return a;
+
+        return a - b;
+    };
+    calcularMultiplicacio = (a, b) => {
+
+        return a * b;
+    };
+
+    calcularDivisio = (a, b) => {
+        if (b === 0) return "No es pot fer l'operació entre 0";
+        return a / b;
+    };
+}
+
+module.exports = Calculadora;
+
+//Crea els tests corresponents per verificar el funcionament de l'exercici Async / Await Nivell 1 - Exercici 1
+
+let employee = new Object();
+
+let getEmployee = (id) => {
+  return new Promise((resolved, rejected) => {
+    let index_treballador = employees.findIndex(
+      (employee) => employee.id === id
+    );
+    if (index_treballador >= 0) {
+      resolved(employees[index_treballador]);
+    } else {
+      rejected("Error, no hi ha cap dada de cap treballador");
+    }
+  });
+};
+
+getEmployee(1)
+  .then((resolved) => {
+    console.log(resolved);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+let getSalary = (employee) => {
+  return new Promise((resolved, rejected) => {
+    let index = 0;
+    for (index; index < salaries.length; index++) {
+      if (salaries[index].id == employee.id) {
+        resolved(salaries[index].salary);
+        break;
+      }
+      if (index >= salaries.length) {
+        rejected("Empleat no trobat");
+      }
+    }
+  });
+};
+
+let employee1 = new Object();
+employee1.id = 3;
+getSalary(employee1)
+  .then((salary) => console.log("L'Empleat te un salari de: " + salary))
+  .catch((rejected) => console.error(rejected));
+
+
+//Crea els tests corresponents per verificar el funcionament de l'exercici Async / Await Nivell 2 - Exercici 1
+
+
+asyncFunction2 = () => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("2 seconds later..."), 2000)
+    })
+}
+
+async function asyncFunction2() {
+    console.log(await asyncFunction2());
+}
+
+asyncFunction2();
+//Crea els tests corresponents per verificar el funcionament de l'exercici Promises & Callbacks Nivell 2 - Exercici 3
+
+let getEmployee2 = id => {
     return new Promise((resolve, reject) => {
         let findEmployee = employees.find(employee => employee.id === id)
         if (findEmployee) {
             resolve(findEmployee.name);
         } else {
-            reject(`The ID ${id} doesn't value, you need put a numbrer from 1 to 3`);
+            reject(`El ID ${id} no es valido, tiene que ser entre 1 y 3`);
         }
     })
 }
 
-getEmployee(2)
-    .then(resolve => console.log(resolve))
-    .catch(err => console.log(err))
-
-let getSalary = (id) => {
+let getSalary2 = (id) => {
     return new Promise((resolve, reject) => {
         let findSalary = salaries.find(salario => salario.id === id);
         if (findSalary) {
@@ -25,12 +114,23 @@ let getSalary = (id) => {
     });
 };
 
-getSalary(2)
-    .then(good => console.log(good))
-    .catch(err => console.log(err));
 
+// N2 E3: Invoca la primera funció getEmployee i després getSalary niant l'execució de les dues promises.
+
+// TODO Invocar la funció getEmployee i després getSalari [x]
+// TODO Niar les dues promeses [x]
+
+let id = 1
+getEmployee2(id).then(name => {
+    console.log(name)
+    return getSalary2(id)
+}).then(salary => {
+    console.log(salary)
+});
 
 module.exports = {
-    getEmployee,
-    getSalary
-};
+    getEmployee2,
+    getSalary2
+}
+
+//Verifica mitjançant tests l'execució de l'exercici Async / Await Nivell 2 Exercici 1 utilitzant Jest Fake Timers.
