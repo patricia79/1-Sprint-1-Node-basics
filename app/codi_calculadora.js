af14533c5eddaf34e5e5b7b1102a746a3e3d7ec1
Patricia <waltraute@hotmail.com>
2022-06-02 22:42:44 +0200

    actualizar

--- app/codi_calculadora.js
+++ app/codi_calculadora.js
@@ -31,49 +31,74 @@ module.exports = Calculadora;
 
 //Crea els tests corresponents per verificar el funcionament de l'exercici Async / Await Nivell 1 - Exercici 1
 
+
+let employees = [{
+  id: 1,
+  name: 'Linux Torvalds'
+}, {
+  id: 2,
+  name: 'Bill Gates'
+}, {
+  id: 3,
+  name: 'Jeff Bezos'
+}];
+
+let salaries = [{
+  id: 1,
+  salary: 4000
+}, {
+  id: 2,
+  salary: 1000
+}, {
+  id: 3,
+  salary: 2000
+}];
 let employee = new Object();
 
 let getEmployee = (id) => {
-  return new Promise((resolved, rejected) => {
-    let index_treballador = employees.findIndex(
-      (employee) => employee.id === id
-    );
-    if (index_treballador >= 0) {
-      resolved(employees[index_treballador]);
-    } else {
-      rejected("Error, no hi ha cap dada de cap treballador");
-    }
-  });
+return new Promise((resolve, reject) => {
+  let index_treballador = employees.findIndex(
+    (employee) => employee.id === id
+  );
+  if (index_treballador >= 0) {
+    resolve(employees[index_treballador]);
+  } else {
+    reject("Error, no hi ha cap dada de cap treballador");
+  }
+});
 };
 
 getEmployee(1)
-  .then((resolved) => {
-    console.log(resolved);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+.then((resolve) => {
+  console.log(resolve);
+})
+.catch((error) => {
+  console.error(error);
+});
 
 let getSalary = (employee) => {
-  return new Promise((resolved, rejected) => {
-    let index = 0;
-    for (index; index < salaries.length; index++) {
-      if (salaries[index].id == employee.id) {
-        resolved(salaries[index].salary);
-        break;
-      }
-      if (index >= salaries.length) {
-        rejected("Empleat no trobat");
-      }
+return new Promise((resolve, reject) => {
+  let index = 0;
+ 
+
+  
+  for (index; index < salaries.length; index++) {
+    if (salaries[index].id == employee.id) {
+      resolve(salaries[index].salary);
+      break;
     }
-  });
+    if (index >= salaries.length) {
+      reject("Empleat no trobat");
+    }
+  }
+});
 };
 
 let employee1 = new Object();
 employee1.id = 3;
 getSalary(employee1)
-  .then((salary) => console.log("L'Empleat te un salari de: " + salary))
-  .catch((rejected) => console.error(rejected));
+.then((salary) => console.log("L'Empleat te un salari de: " + salary))
+.catch((reject) => console.error(reject));
 
 
 //Crea els tests corresponents per verificar el funcionament de l'exercici Async / Await Nivell 2 - Exercici 1
